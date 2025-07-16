34fba35e2f12d7d677a50db4f590eac336d92e95
kothapallypooja <kothapallypooja254@gmail.com>
2025-07-16 21:24:47 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+l=list(range(1,11))
+print('original list: ',l)
+extracted_list = l[0:5]
+print('Extracted first five elements: ',extracted_list)
+extracted_list.reverse()
+print('reversed extracted elements: ',extracted_list)
